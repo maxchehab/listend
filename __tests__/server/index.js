f7e99bc8244d6cffbcd106c15d984e1d8bb77796
maxchehab <maxchehab@gmail.com>
2017-11-09 19:53:47 -0800

    Initial commit baby!

---
+++ __tests__/server/index.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const server = require("../../server/server");
+
+describe("Test the root path", () => {
+  test("It should response the GET method", async () => {
+    const response = await request(server).get("/api/");
+    expect(response.statusCode).toBe(200);
+  });
+});
---
+++ app.js
@@ -0,0 +1,23 @@
+const server = require("./server/server");
+
+const dev = process.env.NODE_ENV !== "production";
+const next = require("next");
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const { PORT } = require("./lib/constants");
+
+app.prepare().then(() => {
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+  /* eslint-disable no-console */
+  server.listen(PORT, err => {
+    if (err) {
+      throw err;
+    }
+    console.log("> Server ready on http://localhost:" + PORT);
+  });
+});
+
+module.exports = server;
